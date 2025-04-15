52c8bbb3e8f144ab5de2e860e874849fa8762d68
Giang Nguyen <hoanggia@uni-bremen.de>
2025-04-15 23:01:40 +0200

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ export PATH=/usr/bin:/lib/x86_64-linux-gnu
 echo "Building multiverse_server..."
 (cd multiverse_server; make clean; make)
 
-# echo "Building multiverse_client..."
-# (cd multiverse_client; make clean; make)
+echo "Building multiverse_client..."
+(cd multiverse_client; make clean; make)
