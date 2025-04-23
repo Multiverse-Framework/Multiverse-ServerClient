f0f10e4a9331a430a90f9db9966bbf7baaccb0c8
Giang Nguyen <hoanggia@uni-bremen.de>
2025-04-23 19:33:33 +0200

    Add build for MSVC

--- setup.bat
+++ setup.bat
@@ -11,7 +11,7 @@ if not exist "%MSYS2_DIR%" (
     mkdir "%MSYS2_DIR%"
     powershell -NoProfile -Command "curl -o '%MSYS2_DIR%\msys2-x86_64-20241208.exe' 'https://github.com/msys2/msys2-installer/releases/download/2025-02-21/msys2-x86_64-20250221.exe'; %MSYS2_DIR%\msys2-x86_64-20241208.exe in --confirm-command --accept-messages --root %MSYS2_DIR%"
     powershell -NoProfile -Command "%MSYS2_DIR%\msys2_shell.cmd -defterm -here -no-start -c 'pacman -Syu --noconfirm'"
-    powershell -NoProfile -Command "%MSYS2_DIR%\msys2_shell.cmd -defterm -here -no-start -c 'pacman -Sy --noconfirm mingw-w64-x86_64-gcc mingw-w64-x86_64-make mingw-w64-x86_64-cmake mingw-w64-x86_64-zeromq mingw-w64-x86_64-cppzmq mingw-w64-x86_64-jsoncpp'"
+    powershell -NoProfile -Command "%MSYS2_DIR%\msys2_shell.cmd -defterm -here -no-start -c 'pacman -Sy --noconfirm mingw-w64-x86_64-gcc mingw-w64-x86_64-make mingw-w64-x86_64-cmake'"
     powershell -NoProfile -Command "%MSYS2_DIR%\msys2_shell.cmd -defterm -here -no-start -c 'pacman -Syu --noconfirm'"
     echo MSYS2 installation complete.
 ) else (
@@ -28,7 +28,11 @@ cd ..
 
 echo Building multiverse_client...
 cd multiverse_client
-%MINGW32_MAKE_EXE% clean
-%MINGW32_MAKE_EXE%
+%MINGW32_MAKE_EXE% clean -f Makefile.mingw
+%MINGW32_MAKE_EXE% -f Makefile.mingw
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+nmake clean -f Makefile.nmake
+nmake -f Makefile.nmake
 cd ..
 endlocal
