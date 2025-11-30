aec5ef3c47c13af1f1bcc910c4f5a54258b4d582
Giang Nguyen <hoanggia@uni-bremen.de>
2025-11-30 22:04:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "Building multiverse_server_rust..."
-(cd multiverse_server_rust || exit; make clean; make install)
+if command -v cargo >/dev/null 2>&1; then
+    echo "Building multiverse_server_rust..."
+    (cd multiverse_server_rust || exit; make clean; make install)
+fi
 
 export PATH=/usr/bin
 
-echo "Building multiverse_server..."
-(cd multiverse_server || exit; make clean; make)
+echo "Building multiverse_server_cpp..."
+(cd multiverse_server_cpp || exit; make clean; make)
 
 echo "Building multiverse_client..."
 (cd multiverse_client || exit; make clean; make)
