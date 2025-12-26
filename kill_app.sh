62fa29fcfa08c091e9a4b21d4a344307cc3122dd
Kien Bui <trungkiena6@gmail.com>
2025-12-26 15:21:59 +0700

    chore: clean up & avoid log folder

--- kill_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Kill the multiverse_server process
-pkill -f multiverse_server
-
-# Kill the app_client process
-pkill -f test_multiverse_client
-
-echo "Processes killed successfully."
